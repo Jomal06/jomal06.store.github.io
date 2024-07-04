aca6bd81927f1c45d4dc6901d732f1fe8c650fda
Jomal06 <120556623+Jomal06@users.noreply.github.com>
2024-07-05 00:00:07 +0800

    Create app.js

---
+++ app.js
@@ -0,0 +1,52 @@
+var product1 = document.getElementById("product1")
+var qty1 = document.getElementById("qty1")
+var price1 = document.getElementById("price1")
+
+var product2 = document.getElementById("product2")
+var qty2 = document.getElementById("qty2")
+var price2 = document.getElementById("price2")
+
+var product3 = document.getElementById("product3")
+var qty3 = document.getElementById("qty3")
+var price3 = document.getElementById("price3")
+
+var product4 = document.getElementById("product4")
+var qty4 = document.getElementById("qty4")
+var price4 = document.getElementById("price4")
+
+var product5 = document.getElementById("product5")
+var qty5 = document.getElementById("qty5")
+var price5 = document.getElementById("price5")
+
+var product6 = document.getElementById("product6")
+var qty6 = document.getElementById("qty6")
+var price6 = document.getElementById("price6")
+
+var product7 = document.getElementById("product7")
+var qty7 = document.getElementById("qty7")
+var price7 = document.getElementById("price7")
+
+var product8 = document.getElementById("product8")
+var qty8 = document.getElementById("qty8")
+var price8 = document.getElementById("price8")
+
+var carts = document.getElementById("carts")
+var total = document.getElementById("total")
+var cash = document.getElementById("cash")
+var change = document.getElementById("change")
+
+function addOrder(){
+  carts.textContent=""
+  if (parseFloat(qty.value) > 0){
+    
+  }
+}
+
+qty1.addEventListener("keyup", addOrder);
+qty2.addEventListener("keyup", addOrder);
+qty3.addEventListener("keyup", addOrder);
+qty4.addEventListener("keyup", addOrder);
+qty5.addEventListener("keyup", addOrder);
+qty6.addEventListener("keyup", addOrder);
+qty7.addEventListener("keyup", addOrder);
+qty8.addEventListener("keyup", addOrder);
