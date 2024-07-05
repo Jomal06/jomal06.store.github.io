17dd39d9b753d72693bd6c2815d374fdf6142200
Jomal06 <120556623+Jomal06@users.noreply.github.com>
2024-07-05 11:06:07 +0800

    Update app.js

--- app.js
+++ app.js
@@ -36,8 +36,8 @@ var cash = document.getElementById("cash");
 var change = document.getElementById("change");
 
 function addOrder(){
-    carts.textContent="";
-    if (parseFloat(qty.value) > 0){
+    carts.textContent = "";
+    if (parseFloat(qty1.value) > 0){
         var order = qty1.value.toString() + ' pc/s x' + price1.textContent + '------' + product1.textContent + '------ Php' + (parseFloat(qty1.value)*parseFloat(price1.textContent)) + '\n';
         //carts.textContent += carts.value.toString() + "\n";
         carts.textContent += order;
